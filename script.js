5c1848c461b9bbcc6a449715ad8a380c22d3fe05
kuwakuwa1225 <kiiroi903@gmail.com>
2026-06-23 11:08:41 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+table { border-collapse: collapse; width: 100%; margin-top: 20px; }
+th, td { border: 1px solid #ccc; padding: 10px; text-align: center; }
+th { background: #f4f4f4; }
+tr:nth-child(even) { background: #f9f9f9; }
